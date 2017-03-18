662764b5e34721fe84abf641a3604f249580bd6e
tafice <tafice@outlook.com>
2017-03-18 15:24:02 +0800

    init

---
+++ NSoup-UWP/Nodes/XmlDeclaration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSoup_UWP.Nodes
+{
+    /// <summary>
+    /// An XML Declaration.
+    /// </summary>
+    /// <!--
+    /// Original Author: Jonathan Hedley, jonathan@hedley.net
+    /// Ported to .NET by: Amir Grozki
+    /// -->
+    public class XmlDeclaration : Node
+    {
+        private static readonly string DECL_KEY = "declaration";
+        private readonly bool _isProcessingInstruction; // <! if true, <? if false, declaration (and last data char should be ?)
+
+        /// <summary>
+        /// Create a new XML declaration
+        /// </summary>
+        /// <param name="data">data</param>
+        /// <param name="baseUri">base uri</param>
+        /// <param name="isProcessingInstruction">is processing instruction</param>
+        public XmlDeclaration(string data, string baseUri, bool isProcessingInstruction)
+            : base(baseUri)
+        {
+            Attributes.Add(DECL_KEY, data);
+            this._isProcessingInstruction = isProcessingInstruction;
+        }
+
+        /// <summary>
+        /// Gets the node's name.
+        /// </summary>
+        public override string NodeName
+        {
+            get { return "#declaration"; }
+        }
+
+        /// <summary>
+        /// Get the unencoded XML declaration.
+        /// </summary>
+        /// <returns>XML declaration</returns>
+        public string GetWholeDeclaration()
+        {
+            return Attributes.GetValue(DECL_KEY);
+        }
+
+        public override void OuterHtmlHead(StringBuilder accum, int depth, OutputSettings output)
+        {
+            accum
+                .Append("<")
+                .Append(_isProcessingInstruction ? "!" : "?")
+                .Append(GetWholeDeclaration())
+                .Append(">");
+        }
+
+        public override void OuterHtmlTail(StringBuilder accum, int depth, OutputSettings output) { }
+
+        public new string ToString()
+        {
+            return OuterHtml();
+        }
+    }
+}
---
+++ NSoup-UWP/Parse/ParseError.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSoup_UWP.Parse
+{
+    /// <summary>
+    /// A Parse Error records an error in the input HTML that occurs in either the tokenisation or the tree building phase.
+    /// </summary>
+    // todo: currently not ready for public consumption. revisit api, and exposure methods
+    public class ParseError
+    {
+
+        private int _pos;
+        private string _errorMsg;
+
+        public ParseError(int pos, string errorMsg)
+        {
+            this._pos = pos;
+            this._errorMsg = errorMsg;
+        }
+
+        public ParseError(int pos, string errorFormat, params object[] args)
+        {
+            this._errorMsg = string.Format(errorFormat, args);
+            this._pos = pos;
+        }
+
+        public string ErrorMessage
+        {
+            get { return _errorMsg; }
+        }
+
+        public int Position
+        {
+            get { return _pos; }
+        }
+
+        public override string ToString()
+        {
+            return _pos + ": " + _errorMsg;
+        }
+    }
+}
